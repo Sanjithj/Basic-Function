6f441dd73392cd5c8c76251a2651f474c84d07d1
Sanjithj <sanjithj591997@gmail.com>
2022-08-14 10:15:51 +0530

    Added: compute quotient and remainder

---
+++ ComputeQandR.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+public class ComputeQandR {
+	public static void main(String[] args) {
+
+	    int dividend = 30;
+	    int divisor = 5;
+
+	    int quotient = dividend / divisor;
+	    int remainder = dividend % divisor;
+
+	    System.out.println("Quotient = " + quotient);
+	    System.out.println("Remainder = " + remainder);
+	  }
+
+}
